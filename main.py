6fe26a7be0d9e874bd032f12c9774e6a87182340
sulsul2 <18221110@std.stei.itb.ac.id>
2023-12-02 22:45:32 +0700

    Middleware CORS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.location import loc
 from routes.order import order
@@ -10,3 +11,13 @@ app = FastAPI()
 app.include_router(loc)
 app.include_router(order)
 app.include_router(auth)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
